4b03fcde112d6e8141a98baee91e590e4550b962
Linnéa Mitchell <linneamitchell@tsf-476-wpa-2-194.epfl.ch>
2026-03-18 17:04:31 +0100

    bonne chance la s

--- balls.h
+++ balls.h
@@ -1,10 +1,17 @@
 // message philosophique: saute dans une flaque avant que la flaque te saute
 #include <vector>
 
+void verif_balls(int& x, int& y, int& r, int& dx, int& dy);
+
 class Ball {
     public : 
     private :
-    vector <int> centre; 
-    vector <int> delta; 
-//tester delta_norm
+    double centre_x;
+    double centre_y;
+    double r;
+    double dx;
+    double dy;
+
 };
+
+vector<Ball> stock; 
